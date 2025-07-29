9085bedb95a79620d5aed32eba43aecc9fd5c8d1
AadilKhan0461 <aadilkhan04610461@gmail.com>
2025-07-29 15:23:11 +0530

    vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./DB/db");
 require("dotenv").config();
-const cors = require('cors')
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,14 +9,32 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors())
+app.use(cors());
 // Or customize CORS
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true
+// }));
 
+app.use(
+  cors({
+    origin: [
+      "https://expense-manager-backend-1-c3hh.onrender.com/",
+      "http://localhost:5173",
+      "https://income-manager-two.vercel.app/",
+    ],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
 
 const startServer = async () => {
   await connectDB(); // wait until DB connects
